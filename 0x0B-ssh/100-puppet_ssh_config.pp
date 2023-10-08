a7a9c5f715b7abc58ae5f1af3bf603fb97eab713
Anthony <bondolo90@gmail.com>
2023-10-08 06:28:26 -0700

    New Server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,14 @@
-# Seting up my client config file
-include stdlib
+#  Config SSH config file for automated passwordless connection using Puppet
 
+include stdlib
 file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'Delare identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
   replace => true,
 }
